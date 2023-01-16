76d9e0cf43477cc127ddd134ee936d9212ee4a5c
rene <rene.polak@gmxat>
2023-01-16 14:24:43 +0100

    First Commit

---
+++ backend/gameverse_backend/src/main/java/com/waff/gameverse_backend/GameverseBackendApplication.java
@@ -0,0 +1,13 @@
+package com.waff.gameverse_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameverseBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameverseBackendApplication.class, args);
+	}
+
+}
---
+++ backend/gameverse_backend/src/test/java/com/waff/gameverse_backend/GameverseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.waff.gameverse_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameverseBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
