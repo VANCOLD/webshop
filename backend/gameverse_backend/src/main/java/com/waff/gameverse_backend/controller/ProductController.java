ef3c4e45b7ee4a7b7d6ff7bc15b49e3f95dc5995
vancold <rene.polak@gmx.at>
2023-02-16 22:12:01 +0100

    Added remaining  services, repos and controller

--- backend/gameverse_backend/src/main/java/com/waff/gameverse_backend/controller/ProductController.java
+++ backend/gameverse_backend/src/main/java/com/waff/gameverse_backend/controller/ProductController.java
@@ -1,19 +1,11 @@
 package com.waff.gameverse_backend.controller;
 
-import com.waff.gameverse_backend.datamodel.Product;
-import com.waff.gameverse_backend.repository.*;
 import com.waff.gameverse_backend.service.ProductService;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.net.URI;
-import java.util.List;
-
 
 @RestController
 @RequestMapping("/products")
