83aa083edabee6730363c870dd0f8a9ecf5bd29a
raja <rajaabdulhadi02@gmail.com>
2023-02-17 11:31:21 +0100

    userService/-Controller fertig geschrieben.

--- backend/gameverse_backend/src/main/java/com/waff/gameverse_backend/controller/UserController.java
+++ backend/gameverse_backend/src/main/java/com/waff/gameverse_backend/controller/UserController.java
@@ -1,12 +1,15 @@
 package com.waff.gameverse_backend.controller;
 
+import com.waff.gameverse_backend.datamodel.User;
 import com.waff.gameverse_backend.service.UserService;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/users")
@@ -15,5 +18,23 @@ public class UserController
 {
 
     private UserService userService;
+
+    @PostMapping("/create")
+    public User createUser(@RequestBody User user){
+        return userService.createUser(user);
+    }
+
+    @DeleteMapping("/delete")
+    public User deleteUser(@RequestBody User user){
+        return userService.deleteUser(user);
+    }
+
+    @GetMapping("/{id}")
+    public Optional<User> findUser(@PathVariable Long uId){return userService.findUserById(uId); }
+
+    @GetMapping
+    public List<User> findAllUsers(){
+        return userService.findAllUsers();
+    }
 
 }
