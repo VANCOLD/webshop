142907f8d2369213aee4ab6af6469a5b2916b9cb
flo <f.grafl@hotmail.de>
2023-09-22 11:08:18 +0200

    https://github.com/VANCOLD/webshop/issues/20 Issue #20 Added Jasypt Encryption for better security

--- backend/gameverse_backend/src/test/java/com/waff/gameverse_backend/service/PrivilegeServiceTest.java
+++ backend/gameverse_backend/src/test/java/com/waff/gameverse_backend/service/PrivilegeServiceTest.java
@@ -9,7 +9,6 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.annotation.DirtiesContext;
 
-import java.util.List;
 import java.util.NoSuchElementException;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
