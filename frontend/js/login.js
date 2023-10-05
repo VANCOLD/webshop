c67c816f5844b8650fe3f562ecc525a2181141fc
flo <f.grafl@hotmail.de>
2023-10-05 16:53:24 +0200

    made changes to design and added validation

--- frontend/js/login.js
+++ frontend/js/login.js
@@ -1,83 +1,117 @@
 $(document).ready(function() {
-    // Event handler for the login form submission
-    $("#login").click(function(event) {
-      // Prevent the default form submission
-      event.preventDefault();
-  
-      // Get the form data
-      var username = $("#usernameLogin").val();
-      var password = $("#passwordLogin").val();
-  
-      // Create an object with the login data
-      var loginData = {
-        username: username,
-        password: password
-      };
-      
-      console.log(loginData);
-  
-      // Send an AJAX POST request to your backend
-      $.ajax({
-        url: "http://localhost:8080/authenticate", 
-        method: 'POST',
-        data: JSON.stringify(loginData),
-        contentType: 'application/json', 
-        dataType: 'text', 
-        success: (response) => {
-            const accessToken = response;
-            localStorage.setItem('token', accessToken); 
-            window.location.href = "index.html?token=" + accessToken;
-        },
-        error: (err) => {
-            console.log(err);
-            alert("Loging in user was unsuccessful!");
-            console.error(err, "Login fehlgeschlagen!");
-        }
-      });
-    });
+  // Event handler for the login form submission
+  $("#login").click(function(event) {
+    // Prevent the default form submission
+    event.preventDefault();
 
+    // Get the form data
+    var username = $("#usernameLogin").val();
+    var password = $("#passwordLogin").val();
 
-    $("#register").click(function(event) {
-      // Prevent the default form submission
-      event.preventDefault();
+    // Reset validation classes and error message
+    $("#usernameLogin").removeClass("is-invalid");
+    $("#passwordLogin").removeClass("is-invalid");
+    $("#loginErrorMessage").hide();
 
-        // Get the form data
-      var username = $("#username").val();
-      var password = $("#password").val();
-  
-      // Create an object with the login data
-      var registerData = {
-        username: username,
-        password: password
-      };
-      
-  
-      $.ajax({
-        url: "http://localhost:8080/register", 
-        method: 'POST',
-        data: JSON.stringify(registerData),
-        contentType: 'application/json', 
-        dataType: 'text', 
-        success: (response) => {
-
-            // Assume 'response' is the JSON response received from the backend
-            var jsonObject = JSON.parse(response);
-            updateUser(jsonObject.id);
-            alert("Registered user successfully!\nPlease log in");
-
-        },
-        error: (err) => {
-            console.error(err, "Login fehlgeschlagen!");
-            alert("Registered user unsuccessful!");
-        }
-      });
-      
-    });
-
-    function updateUser(userid) {
-      var updateData = {
-        username: username,
-        password: password
-      };
+    // Perform validation checks
+    var valid = true;
+    if (username === "") {
+      $("#usernameLogin").addClass("is-invalid");
+      valid = false;
     }
+    if (password === "") {
+      $("#passwordLogin").addClass("is-invalid");
+      valid = false;
+    }
+
+    if (!valid) {
+      $("#loginErrorMessage").show();
+      return;
+    }
+
+    // Create an object with the login data
+    var loginData = {
+      username: username,
+      password: password
+    };
+
+    // Send an AJAX POST request to your backend
+    $.ajax({
+      url: "http://localhost:8080/authenticate",
+      method: 'POST',
+      data: JSON.stringify(loginData),
+      contentType: 'application/json',
+      dataType: 'text',
+      success: (response) => {
+        const accessToken = response;
+        localStorage.setItem('token', accessToken);
+        window.location.href = "index.html?token=" + accessToken;
+      },
+      error: (err) => {
+        console.log(err);
+        alert("Login unsuccessful. Please check your credentials.");
+        console.error(err, "Login fehlgeschlagen!");
+      }
+    });
   });
+
+  // Event handler for the registration form submission
+  $("#register").click(function(event) {
+    // Prevent the default form submission
+    event.preventDefault();
+
+    // Get the form data
+    var username = $("#username").val();
+    var password = $("#password").val();
+
+    // Reset validation classes and error message
+    $("#username").removeClass("is-invalid");
+    $("#password").removeClass("is-invalid");
+    $("#registerErrorMessage").hide();
+
+    // Perform validation checks
+    var valid = true;
+    if (username === "") {
+      $("#username").addClass("is-invalid");
+      valid = false;
+    }
+    if (!/^[A-Za-z0-9]+$/.test(username)) {
+      $("#username").addClass("is-invalid");
+      valid = false;
+    }
+    if (password === "") {
+      $("#password").addClass("is-invalid");
+      valid = false;
+    }
+    
+    if (!valid) {
+      $("#registerErrorMessage").show();
+      return;
+    }
+
+    // Create an object with the registration data
+    var registerData = {
+      username: username,
+      password: password
+    };
+
+    // Send an AJAX POST request to your backend
+    $.ajax({
+      url: "http://localhost:8080/register",
+      method: 'POST',
+      data: JSON.stringify(registerData),
+      contentType: 'application/json',
+      dataType: 'text',
+      success: (response) => {
+        // Assume 'response' is the JSON response received from the backend
+        var jsonObject = JSON.parse(response);
+        updateUser(jsonObject.id);
+        alert("Registered user successfully!\nPlease log in");
+      },
+      error: (err) => {
+        console.error(err, "Registration fehlgeschlagen!");
+        alert("Registration unsuccessful. Please try again.");
+      }
+    });
+  });
+});
