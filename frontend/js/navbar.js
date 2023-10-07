3b6b43f13b08b855a4ba16b64a386761f6c543f3
Rene <v-i-p-e-r@gmx.at>
2023-10-07 14:27:52 +0200

    Buttons show / hide if you dont have the correct privileges

--- frontend/js/navbar.js
+++ frontend/js/navbar.js
@@ -1,10 +1,69 @@
+const profile   = document.getElementById('profile-button');
+const login     = document.getElementById('register-button');
+const cart      = document.getElementById('cart-button');
+const logout    = document.getElementById('logout-button');
+
+const viewProfile = 'view_profile';   
+const viewCart = 'view_cart';           
+  
+document.getElementById('logout-button').addEventListener('click', function (e) {
+    localStorage.setItem('token','');
+});
+
 $(document).ready(function() {
 
     // Get token
     var token = localStorage.getItem("token");
+    console.log(token)
 
-    if(token == null) {
-        alert("kein token gefunden");
+    if(token == '') {
+        profile.style.display = 'none';
+        cart.style.display = 'none';
+        logout.style.display = 'none';
+
+    } else {
+        const jwtParser = new JWTParser(token);
+        
+        try {
+            const payload = jwtParser.parse();
+            const privs   = payload.authority.split(' ');
+
+            if (privs.includes(viewProfile)) {
+                profile.style.display = 'visible';
+            }
+
+            if(privs.includes(viewCart)) {
+                profile.style.display = 'visible';
+            }
+
+
+            login.style.display = 'none';
+
+        } catch (error) {
+            console.log('Error parsing JWT:', error.message);
+        }
     }
 
 });
+
+
+class JWTParser {
+    constructor(token) {
+      this.token = token;
+    }
+  
+    parse() {
+        try {
+            const parts = this.token.split('.');
+            if (parts.length !== 3) {
+            throw new Error('Invalid token format');
+            }
+    
+            const payload = JSON.parse(atob(parts[1]));
+            return payload;
+        } catch (error) {
+            throw new Error('Invalid token: ' + error.message);
+        }
+    }
+}
+  
