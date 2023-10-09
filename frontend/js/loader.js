6abba2afc7d581e087018ae798aac607a49c57c0
rene <rene.polak@gmx.at>
2023-10-09 10:13:51 +0200

    test

--- frontend/js/loader.js
+++ frontend/js/loader.js
@@ -3,17 +3,19 @@ $(document).ready(function() {
 
     // Use jQuery to fetch the external HTML file
     $.ajax({
-        url: '../navbar.html', // Replace with the path to your external HTML file
+        url: 'navbar.html', // Replace with the path to your external HTML file
         dataType: 'html',
         success: function(data) {
             // Insert the content into the specified element
             $('#navbar-bind').replaceWith(data);
+            
+            (data);
         }
     });
 
     // Use jQuery to fetch the external HTML file
     $.ajax({
-        url: '../footer.html', // Replace with the path to your external HTML file
+        url: 'footer.html', // Replace with the path to your external HTML file
         dataType: 'html',
         success: function(data) {
             // Insert the content into the specified element
