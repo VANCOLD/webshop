d488b4e0dd8a4cb494045998b679b843f57c1271
flo <f.grafl@hotmail.de>
2023-10-11 13:03:28 +0200

    added js and CartController, CartService

--- backend/gameverse_backend/src/main/java/com/waff/gameverse_backend/controller/CartController.java
+++ backend/gameverse_backend/src/main/java/com/waff/gameverse_backend/controller/CartController.java
@@ -1,19 +1,20 @@
 package com.waff.gameverse_backend.controller;
 import com.waff.gameverse_backend.dto.AddProductToCartDto;
 import com.waff.gameverse_backend.model.Cart;
-import com.waff.gameverse_backend.model.User;
+import com.waff.gameverse_backend.model.CartItem;
 import com.waff.gameverse_backend.service.CartService;
 import com.waff.gameverse_backend.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 
 @RestController
 @RequestMapping("/api/cart")
 public class CartController {
     private final CartService cartService;
-
     private final UserService userService;
 
     @Autowired
@@ -27,15 +28,65 @@ public class CartController {
         Long userId = requestDto.getUserid();
         Long productId = requestDto.getProductid();
 
-        // Call the UserService to get the current user (you need to create UserService if not already done)
-        User User = userService.findById(userId);
-
         // Call the CartService to add the product to the user's cart
-        Cart updatedCart = cartService.addToCart(User.getId(), productId);
+        Cart updatedCart = cartService.addToCart(userId, productId);
 
         return ResponseEntity.ok(updatedCart);
     }
 
+    @GetMapping("/get")
+    public ResponseEntity<Cart> getCartContents(@RequestParam Long userId) {
+        // Call the CartService to get the user's cart contents
+        Cart userCart = cartService.getCartByUserId(userId);
+        return ResponseEntity.ok(userCart);
+    }
 
+    @DeleteMapping("/remove")
+    public ResponseEntity<Cart> removeFromCart(@RequestParam Long userId, @RequestParam Long productId) {
+        // Call the CartService to remove the product from the user's cart
+        Cart updatedCart = cartService.removeFromCart(userId, productId);
+        return ResponseEntity.ok(updatedCart);
+    }
 
+    @DeleteMapping("/clear")
+    public ResponseEntity<Void> clearCart(@RequestParam Long userId) {
+        // Call the CartService to clear the user's cart
+        cartService.clearCart(userId);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<Cart> updateCartItemQuantity(@RequestParam Long userId, @RequestParam Long productId, @RequestParam int quantity) {
+        // Call the CartService to update the quantity of the product in the user's cart
+        Cart updatedCart = cartService.updateCartItemQuantity(userId, productId, quantity);
+        return ResponseEntity.ok(updatedCart);
+    }
+
+    @GetMapping("/total")
+    public ResponseEntity<Double> calculateCartTotal(@RequestParam Long userId) {
+        // Call the CartService to calculate the total price of the user's cart
+        Double total = cartService.calculateCartTotal(userId);
+        return ResponseEntity.ok(total);
+    }
+
+    @GetMapping("/count")
+    public ResponseEntity<Integer> getCartItemCount(@RequestParam Long userId) {
+        // Call the CartService to get the total number of items in the user's cart
+        Integer itemCount = cartService.getCartItemCount(userId);
+        return ResponseEntity.ok(itemCount);
+    }
+
+    @GetMapping("/items")
+    public ResponseEntity<List<CartItem>> getCartItems(@RequestParam Long userId) {
+        // Call the CartService to get the list of cart items in the user's cart
+        List<CartItem> cartItems = cartService.getCartItems(userId);
+        return ResponseEntity.ok(cartItems);
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<Cart>> getAllCarts() {
+        // Call the CartService to get a list of all carts
+        List<Cart> allCarts = cartService.getAll();
+        return ResponseEntity.ok(allCarts);
+    }
 }
--- backend/gameverse_backend/src/main/java/com/waff/gameverse_backend/service/CartService.java
+++ backend/gameverse_backend/src/main/java/com/waff/gameverse_backend/service/CartService.java
@@ -1,6 +1,5 @@
 package com.waff.gameverse_backend.service;
 
-import com.waff.gameverse_backend.dto.CartDto;
 import com.waff.gameverse_backend.model.Cart;
 import com.waff.gameverse_backend.model.CartItem;
 import com.waff.gameverse_backend.model.Product;
@@ -8,22 +7,21 @@ import com.waff.gameverse_backend.model.User;
 import com.waff.gameverse_backend.repository.CartRepository;
 import com.waff.gameverse_backend.repository.ProductRepository;
 import com.waff.gameverse_backend.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Collections;
 import java.util.List;
 import java.util.NoSuchElementException;
-import java.util.Optional;
 
 @Service
 public class CartService {
-
     private final CartRepository cartRepository;
-
     private final UserRepository userRepository;
-
     private final ProductRepository productRepository;
+    private static final Logger logger = LoggerFactory.getLogger(CartService.class);
 
     @Autowired
     public CartService(CartRepository cartRepository, UserRepository userRepository, ProductRepository productRepository) {
@@ -33,132 +31,166 @@ public class CartService {
     }
 
     public Cart getCartByUserId(Long userId) {
-        // Use the findByUserId method from the repository to fetch carts by user ID
-        User user = this.userRepository.findById(userId).orElseThrow(
-                () -> new NoSuchElementException("User existiert nicht!"));
+        try {
+            User user = userRepository.findById(userId)
+                    .orElseThrow(() -> new NoSuchElementException("User not found"));
 
-        // You can handle the result here, e.g., return the list of carts
-        return user.getCart();
+            return user.getCart();
+        } catch (NoSuchElementException ex) {
+            logger.error("Error while getting the user's cart: " + ex.getMessage());
+            throw ex;
+        }
     }
 
     public Cart addToCart(Long userId, Long productId) {
-        // Retrieve the user's cart by userId
-        Cart userCart = this.getCartByUserId(userId);
+        try {
+            User user = userRepository.findById(userId)
+                    .orElseThrow(() -> new NoSuchElementException("User not found"));
+            Product product = productRepository.findById(productId)
+                    .orElseThrow(() -> new NoSuchElementException("Product not found"));
 
-        // Check if the user has a cart, if not, create a new one
-        if (userCart == null) {
-            userCart = new Cart();
-            userCart.setUser(userRepository.findById(userId).orElse(null)); // Assuming you have a userRepository
-        }
+            Cart userCart = user.getCart();
 
-        // Retrieve the product by productId
-        Product product = productRepository.findById(productId).orElse(null); // Assuming you have a productRepository
-
-        // Create a flag to check if the item already exists in the cart
-        boolean itemExists = false;
-
-        // Iterate through the cart items to find if the product already exists
-        for (CartItem existingItem : userCart.getProducts()) {
-            if (existingItem.getProduct().getId().equals(product.getId())) {
-                // If the product already exists, update the amount
-                existingItem.setAmount(existingItem.getAmount() + 1);
-                itemExists = true;
-                break;  // No need to continue iterating
+            if (userCart == null) {
+                userCart = new Cart();
+                userCart.setUser(user);
             }
-        }
 
-        if (!itemExists) {
-            // If the item doesn't exist in the cart, create a new one
-            CartItem newItem = new CartItem();
-            newItem.setProduct(product);
-            newItem.setCart(userCart);
-            newItem.setAmount(1); // Set the amount to 1 for a new item
-            userCart.getProducts().add(newItem);
-        }
+            boolean itemExists = false;
 
-        // Save the updated cart
-        return cartRepository.save(userCart);
+            for (CartItem existingItem : userCart.getProducts()) {
+                if (existingItem.getProduct().getId().equals(product.getId())) {
+                    existingItem.setAmount(existingItem.getAmount() + 1);
+                    itemExists = true;
+                    break;
+                }
+            }
+
+            if (!itemExists) {
+                CartItem newItem = new CartItem();
+                newItem.setProduct(product);
+                newItem.setCart(userCart);
+                newItem.setAmount(1);
+                userCart.getProducts().add(newItem);
+            }
+
+            return cartRepository.save(userCart);
+        } catch (NoSuchElementException ex) {
+            logger.error("Error while adding a product to the cart: " + ex.getMessage());
+            throw ex;
+        }
     }
 
     public Cart removeFromCart(Long userId, Long productId) {
-        // Retrieve the user's cart by userId
-        Cart userCart = this.getCartByUserId(userId);
+        try {
+            User user = userRepository.findById(userId)
+                    .orElseThrow(() -> new NoSuchElementException("User not found"));
+            Product product = productRepository.findById(productId)
+                    .orElseThrow(() -> new NoSuchElementException("Product not found"));
 
-        if (userCart == null) {
-            // Handle the case where the user's cart doesn't exist
-            return null;
+            Cart userCart = user.getCart();
+
+            if (userCart == null) {
+                return null;
+            }
+
+            userCart.getProducts().removeIf(item -> item.getProduct().getId().equals(productId));
+
+            return cartRepository.save(userCart);
+        } catch (NoSuchElementException ex) {
+            logger.error("Error while removing a product from the cart: " + ex.getMessage());
+            throw ex;
         }
-
-        // Check if the product is in the cart
-        userCart.getProducts().removeIf(item -> item.getProduct().getId().equals(productId));
-        // Save the updated cart
-        return cartRepository.save(userCart);
     }
 
     public Double calculateCartTotal(Long userId) {
-        // Retrieve the user's cart by userId
-        Cart userCart = this.getCartByUserId(userId);
+        try {
+            User user = userRepository.findById(userId)
+                    .orElseThrow(() -> new NoSuchElementException("User not found"));
+            Cart userCart = user.getCart();
 
-        // Calculate the total price based on the products in the cart
-        double total = userCart.getProducts().stream()
-                .mapToDouble(item -> item.getProduct().getPrice() * item.getAmount())
-                .sum();
-        return total;
+            double total = userCart.getProducts().stream()
+                    .mapToDouble(item -> item.getProduct().getPrice() * item.getAmount())
+                    .sum();
+
+            return total;
+        } catch (NoSuchElementException ex) {
+            logger.error("Error while calculating the cart total: " + ex.getMessage());
+            throw ex;
+        }
     }
 
     public void clearCart(Long userId) {
-        // Retrieve the user's cart by userId
-        Cart userCart = this.getCartByUserId(userId);
+        try {
+            User user = userRepository.findById(userId)
+                    .orElseThrow(() -> new NoSuchElementException("User not found"));
+            Cart userCart = user.getCart();
 
-        // Clear the products from the cart
-        userCart.getProducts().clear();
-        // Save the updated cart
-        cartRepository.save(userCart);
+            userCart.getProducts().clear();
+            cartRepository.save(userCart);
+        } catch (NoSuchElementException ex) {
+            logger.error("Error while clearing the cart: " + ex.getMessage());
+            throw ex;
+        }
     }
 
     public Cart updateCartItemQuantity(Long userId, Long productId, int newQuantity) {
-        // Retrieve the user's cart by userId
-        Cart userCart = this.getCartByUserId(userId);
-        // Find the cart item corresponding to the product
-        CartItem cartItem = userCart.getProducts().stream()
-                .filter(item -> item.getProduct().getId().equals(productId))
-                .findFirst().orElse(null);
+        try {
+            User user = userRepository.findById(userId)
+                    .orElseThrow(() -> new NoSuchElementException("User not found"));
+            Product product = productRepository.findById(productId)
+                    .orElseThrow(() -> new NoSuchElementException("Product not found"));
 
-        if (cartItem != null) {
-            // Update the cart item's quantity
-            cartItem.setAmount(newQuantity);
+            Cart userCart = user.getCart();
+            CartItem cartItem = userCart.getProducts().stream()
+                    .filter(item -> item.getProduct().getId().equals(productId))
+                    .findFirst()
+                    .orElse(null);
+
+            if (cartItem != null) {
+                cartItem.setAmount(newQuantity);
+            }
+
+            return cartRepository.save(userCart);
+        } catch (NoSuchElementException ex) {
+            logger.error("Error while updating the cart item quantity: " + ex.getMessage());
+            throw ex;
         }
-
-        // Save the updated cart
-        return cartRepository.save(userCart);
     }
 
     public int getCartItemCount(Long userId) {
-        // Retrieve the user's cart by userId
-        Cart userCart = this.getCartByUserId(userId);
+        try {
+            User user = userRepository.findById(userId)
+                    .orElseThrow(() -> new NoSuchElementException("User not found"));
+            Cart userCart = user.getCart();
 
-        // Calculate the total number of items in the cart
-        return userCart.getProducts().stream()
-                .mapToInt(CartItem::getAmount)
-                .sum();
+            return userCart.getProducts().stream()
+                    .mapToInt(CartItem::getAmount)
+                    .sum();
+        } catch (NoSuchElementException ex) {
+            logger.error("Error while getting the cart item count: " + ex.getMessage());
+            throw ex;
+        }
     }
 
     public List<CartItem> getCartItems(Long userId) {
-        // Retrieve the user's cart by userId
-        Cart userCart = this.getCartByUserId(userId);
+        try {
+            User user = userRepository.findById(userId)
+                    .orElseThrow(() -> new NoSuchElementException("User not found"));
+            Cart userCart = user.getCart();
 
-        if (userCart == null) {
-            // Handle the case where the user's cart doesn't exist
-            return Collections.emptyList(); // Return an empty list if the cart doesn't exist
+            if (userCart == null) {
+                return Collections.emptyList();
+            }
+
+            return userCart.getProducts();
+        } catch (NoSuchElementException ex) {
+            logger.error("Error while getting cart items: " + ex.getMessage());
+            throw ex;
         }
-
-        // Return the list of cart items
-        return userCart.getProducts();
     }
 
-    public List<Cart> getAll(){
-        return this.cartRepository.findAll();
+    public List<Cart> getAll() {
+        return cartRepository.findAll();
     }
-
 }
-
