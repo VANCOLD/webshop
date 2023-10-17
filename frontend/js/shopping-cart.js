e549e06e854b02f7006b209a85372f87203e71d0
Rene <v-i-p-e-r@gmx.at>
2023-10-17 18:12:07 +0200

    removed order as param from link

--- frontend/js/shopping-cart.js
+++ frontend/js/shopping-cart.js
@@ -160,7 +160,7 @@ function checkout() {
                 'Authorization': `Bearer ${accessToken}`
             },
             success: function(data) {
-                window.location.href = "order.html?order=" + data.id;
+                window.location.href = "order.html";
             },
             error: function(err) {
                 // Handle errors
