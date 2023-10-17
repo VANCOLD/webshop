058d7316880af5ebe25b9c02547f652d2530d0f1
raja <rajaabdulhadi02@gmail.com>
2023-10-17 15:58:36 +0200

    implements a createProduct page and implements it js methods. WIP

--- frontend/js/products.js
+++ frontend/js/products.js
@@ -29,31 +29,30 @@ function loadData() {
         
             const productTemplate = `
                 <div class="container h-auto pb-5">
-                                    <h2 class="text-center pt-3 mb-1" style="margin: 20px;">${product.name}</h2>
-                                    <div class="product-container d-flex align-items-center" style="margin-top: 20px;">
-                                        <img class="product-image col-lg-5" src="${imageSrc}" alt="${product.name}">
-                                        <div>
-                                            <p class="product-description">
-                                            ${product.description}
-                                            </p>
-                                            <div class="product-price">
-                                                ${priceFormatted}
-                                            </div>
-                                            <div class="product-category">
-                                                Category: ${product.category.name}
-                                            </div>
-                                            <div class="product-stock">
-                                                Stock: ${product.stock}
-                                            </div>
-                                            <div>
-                                                <button class="btn btn-buy-now"
-                                                    type="button" onclick="addToCart()">Add to Cart
-                                                </button>
-                                                
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
+                    <h2 class="text-center pt-3 mb-1" style="margin: 20px;">${product.name}</h2>
+                    <div class="product-container d-flex align-items-center" style="margin-top: 20px;">
+                        <img class="product-image col-lg-5" src="${imageSrc}" alt="${product.name}">
+                        <div>
+                            <p class="product-description">
+                            ${product.description}
+                            </p>
+                            <div class="product-price">
+                                ${priceFormatted}
+                            </div>
+                            <div class="product-category">
+                                Category: ${product.category.name}
+                            </div>
+                            <div class="product-stock">
+                                Stock: ${product.stock}
+                            </div>
+                            <div>
+                                <button class="btn btn-buy-now"
+                                    type="button" onclick="addToCart()">Add to Cart
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             `;
         
             productContainer.append(productTemplate);
@@ -97,4 +96,64 @@ function addToCart() {
           }
       });
   }
+}
+
+const form = document.querySelector('form');
+const formData = {
+    name: form.querySelector('input[name="name"]').value,
+    description: form.querySelector('input[name="description"]').value,
+    price: form.querySelector('input[name="price"]').value,
+    image: form.querySelector('input[name="image"]').value,
+    tax: form.querySelector('input[name="tax"]').value,
+    stock: form.querySelector('input[name="stock"]').value,
+    gtin: form.querySelector('input[name="gtin"]').value,
+};
+
+// Add the Authorization header
+formData['headers'] = {
+  'Authorization': `Bearer ${localStorage.getItem('token')}`
+};
+
+saveProduct(formData);
+
+
+function saveProduct(name, description, price, image, tax, stock, gtin) {
+    // Create a JSON object with the product data.
+    const productData = {
+    name,
+    description,
+    price,
+    image,
+    tax,
+    stock,
+    gtin
+    };
+
+    const accessToken = localStorage.getItem('token');
+
+    // Add a headers object to the AJAX call.
+    const headers = {
+        // 'Authorization': `Bearer ${localStorage.getItem('token')}`
+        'Authorization': `Bearer ${accessToken}`
+    };
+    // Make an AJAX call to the backend method that saves a new product.
+    $.ajax({
+      type: 'POST',
+      url: 'http://localhost:8080/api/products',
+      headers,
+      data: JSON.stringify(productData),
+      contentType: 'application/json',
+      success: function() {
+        // Display a message to the user that the product has been saved successfully.
+        alert('Product saved successfully!');
+      },
+      error: function(err) {
+        // Log the error to the console.
+        console.error('Error saving product: ', err);
+      }
+    });
+}
+
+function createProduct() {
+    window.open('/createProduct.html', '_blank');
 }
